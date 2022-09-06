5d758f9b5431c4e655424dd454cdfac83e6822aa
nastyabekesheva <nastyabekesheva@gmail.com>
2022-09-06 20:02:49 +0300

    working on channels

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ def parse(result_collection, id):
                 if 'Кохтич' in messages[6][i]:
                     messages[6][i] = messages[6][i].replace('\U0001f9d1\U0001f3fb\u200D\U0001f3eb', '\u267F')
                     
-        if r['time'] == '19:59':
+        if r['time'] == '20:10':
             try:
                 messages[7].append(f'6\uFE0F\u20E3 пара (18:30)\n{r["name"]}\n\U0001f4dc {r["type"]}\n\U0001f9d1\U0001f3fb\u200D\U0001f3eb {r["teacher"]}\nПосилання: <a href="{r["link"]}">тут</a>\n\n')
             except KeyError:
@@ -387,7 +387,7 @@ def main():
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(11,10))
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(13,5))
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(15,25))
-    job_daily = j.run_daily(c, days=(0, 1, 2, 3, 4, 5), time=datetime.time(16,59))
+    job_daily = j.run_daily(c, days=(0, 1, 2, 3, 4, 5), time=datetime.time(17,5))
     job_daily = j.run_daily(morning_notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(4,59))
     
     
