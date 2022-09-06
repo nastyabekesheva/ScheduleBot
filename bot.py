6ed637e5695a14aa3acc78282fd3b7e4217a15dd
nastyabekesheva <nastyabekesheva@gmail.com>
2022-09-06 18:42:34 +0300

    fix

--- bot.py
+++ bot.py
@@ -325,6 +325,7 @@ def notification(context: CallbackContext):
         message = parse(temp, user['chat_id'])
         if message != 'Відпочивай':
             context.bot.send_message(chat_id=user['chat_id'], text=message, parse_mode=ParseMode.HTML)
+    bot.send_message(chat_id='-1001638244054', text='bot')
         
 def morning_notification(context: CallbackContext):
     today = datetime.date.today()
@@ -342,7 +343,7 @@ def morning_notification(context: CallbackContext):
         message = parse(result, user['chat_id'])
         if message != 'Відпочивай':
             context.bot.send_message(chat_id=user['chat_id'], text=bold(start_message)+message, parse_mode=ParseMode.HTML)
-    bot.send_message(chat_id='-1001638244054', text='bot')
+
     
 
 def main():
@@ -352,7 +353,7 @@ def main():
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(11,10))
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(13,5))
     job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(15,25))
-    job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(15,41))
+    job_daily = j.run_daily(notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(15,44))
     job_daily = j.run_daily(morning_notification, days=(0, 1, 2, 3, 4, 5), time=datetime.time(4,59))
     
     
