49066b6d06d13bb5cb30386a925aa3bccce99715
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-11-01 20:46:03 +0200

    timezone fixes

--- bot.py
+++ bot.py
@@ -1,11 +1,3 @@
-import pip
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])`
-
-
 from telegram.ext import *
 from telegram import *
 import pymongo
